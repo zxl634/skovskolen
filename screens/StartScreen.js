78444b9d14d89e8e20da8b10d8c82586d717ed59
avriis <avr@moos-bjerre.dk>
2019-11-23 12:56:08 +0100

    Med start-skærm

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -11,26 +11,18 @@ import {
   ImageBackground,
 } from 'react-native';
 
-import { MonoText } from '../components/StyledText';
-
 export default function HomeScreen(props) {
-  function start () {
-    props.navigation.navigate('Main');
-  }
   return (
-    <ImageBackground source={require("../assets/backgrounds/startside.png")} style={{width: '100%', height: '100%'}}>
-      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <View style={{flex: 0.85}}></View>
-        <View style={{flex: 0.11, paddingLeft: 2}}>
-          <TouchableOpacity onPress={start}>
-            <Image
-              style={styles.stretch}
-              source={require('../assets/buttons/startButton.png')}
-            />
-        </TouchableOpacity>
+    <ImageBackground source={require("../assets/backgrounds/startScreen.png")} style={{width: '100%', height: '100%'}}>
+        <View style={styles.startContainer}>
+          <Text style={styles.paragraph}> Velkommen til </Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.StartTuren} //"Start turen"-knap. Linjen nedenfor linker til kortet, når der trykkes på knappen
+              onPress={() => props.navigation.navigate('Main')}>
+              <Text style={styles.startTurenTekst}>Start turen</Text>
+          </TouchableOpacity>
+          </View>
         </View>
-        <View style={{flex: 0.1}}></View>
-      </View>
     </ImageBackground>
   );
 }
@@ -39,92 +31,40 @@ HomeScreen.navigationOptions = {
   tabBarVisible: false,
   header: null,
 };
+  const styles = StyleSheet.create({ //opretter stylesheet
+    paragraph: {
+      margin:75,
+      fontSize: 28,
+      textAlign: 'center',
+      color: 'white',
+      // fontFamily: "Gaegu",
+    },
+    startContainer: {
+      flex: 1, //sætter baggrunden til at fylde hele skærmen
+    },
+    buttonContainer:{
+      flex:1,
+      paddingHorizontal:90,
+      //paddingTop:'80%',
+      paddingBottom: 0,
+      overflow: 'hidden',
+      justifyContent: 'center',
+      marginTop: 260
+    },
+    StartTuren: {
+      color: 'red',
+      backgroundColor: '#fc7355',
+      padding: 15,
+      borderRadius: 20,
+      
+      //paddingHorizontal: 5,
+      
+    },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+    startTurenTekst:{
+      color: "white",
+      textAlign: 'center',
+      fontSize: 20,
+    } 
+  })
+
